9b35627f2fb08e3a83aed8ba582e3b8229905a12
Evgenia Bullis <bulish2015@yandex.ru>
2025-03-20 19:23:10 +0300

    init

---
+++ src/test/java/com/bulish/melnikov/converter/MyVideoConverterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulish.melnikov.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVideoConverterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
