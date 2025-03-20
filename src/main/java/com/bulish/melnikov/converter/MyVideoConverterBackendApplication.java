52baef246bbd7c6b7149be874116e7a07212ede1
Evgenia Bullis <bulish2015@yandex.ru>
2025-03-20 19:23:10 +0300

    init

---
+++ src/main/java/com/bulish/melnikov/converter/MyVideoConverterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bulish.melnikov.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVideoConverterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVideoConverterBackendApplication.class, args);
+	}
+
+}
