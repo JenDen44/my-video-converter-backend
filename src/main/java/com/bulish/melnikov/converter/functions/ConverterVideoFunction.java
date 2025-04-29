5590e15866eb161025e137a5eba3beab7acccbc1
Evgesha-thunder <72350114+Evgesha-thunder@users.noreply.github.com>
2025-04-29 17:30:47 +0300

    process video convert request.
    process video convert request.

---
+++ src/main/java/com/bulish/melnikov/converter/functions/ConverterVideoFunction.java
@@ -0,0 +1,27 @@
+package com.bulish.melnikov.converter.functions;
+
+import com.bulish.melnikov.converter.model.ConvertRequestMsgDTO;
+import com.bulish.melnikov.converter.service.ConverterFileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class ConverterVideoFunction {
+
+    private final ConverterFileService converterFileService;
+
+    @Bean
+    public Consumer<ConvertRequestMsgDTO> convert() {
+        return convertRequest -> {
+            log.info("Request to convert arrived : from " +  convertRequest.getFormatFrom()
+                    + " to " + convertRequest.getFormatTo());
+            converterFileService.requestToConvert(convertRequest);
+        };
+    }
+}
--- src/main/java/com/bulish/melnikov/converter/service/ConverterFileServiceImpl.java
+++ src/main/java/com/bulish/melnikov/converter/service/ConverterFileServiceImpl.java
@@ -1,82 +1,20 @@
 package com.bulish.melnikov.converter.service;
 
-import com.bulish.melnikov.converter.exception.IncorrectFormatExtensionException;
-import com.bulish.melnikov.converter.mapper.ConvertRequestToConvertResponseMapper;
-import com.bulish.melnikov.converter.model.ConvertRequest;
-import com.bulish.melnikov.converter.model.ConvertResponse;
-import com.bulish.melnikov.converter.model.ExtensionDto;
-import com.bulish.melnikov.converter.model.State;
+import com.bulish.melnikov.converter.model.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class ConverterFileServiceImpl implements ConverterFileService {
 
-    private final ConvertRequestService convertRequestService;
     private final ConverterRequestQueueManagerServiceImpl queueService;
-    private final ConvertRequestToConvertResponseMapper requestToConvertResponseMapper;
-    private final FileService fileService;
-    private final ExtensionService extensionService;
 
-    private final String dirUpload = "temp/files/";
 
     @Override
-    public ConvertResponse requestToConvert(MultipartFile fileToConvert, String formatTo) {
-        String formatFrom = fileService.getFileExtension(fileToConvert.getOriginalFilename());
-
-        List<ExtensionDto> extensions = extensionService.getAllowedExtensions();
-        if (!extensions.stream().anyMatch(e -> e.getFormatFrom().equals(formatFrom)
-                && e.getFormatsTo().contains(formatTo))) {
-
-            throw new IncorrectFormatExtensionException("Check available formats formatTo "
-                    + formatTo + " or formatFrom " +  formatFrom + "are not supported");
-        }
-
-        String filePath = null;
-        try {
-            filePath = fileService.saveMultipartFile(fileToConvert);
-        } catch (IOException e) {
-            log.error("Error uploading Multipart File", e);
-            throw new RuntimeException(e);
-        }
-
-        ConvertRequest convertRequest = new ConvertRequest(filePath, formatTo, formatFrom);
-        convertRequestService.save(convertRequest);
-
+    public void requestToConvert(ConvertRequestMsgDTO convertRequest) {
         queueService.addRequestToQueue(convertRequest);
-
-        return requestToConvertResponseMapper.convertRequestToConvertResponse(convertRequest);
-    }
-
-    @Override
-    public ConvertResponse getRequestStatusById(String id) {
-       ConvertRequest convertRequest = convertRequestService.get(id);
-
-        return requestToConvertResponseMapper.convertRequestToConvertResponse(convertRequest);
-    }
-
-    @Override
-    public byte[] downloadConvertedFile(String id) {
-        ConvertRequest convertRequest = convertRequestService.get(id);
-
-        if (convertRequest.getState() == State.CONVERTED) {
-            try {
-                return Files.readAllBytes(Paths.get(convertRequest.getConvertedFilePath()));
-            } catch (IOException e) {
-                throw new RuntimeException("Error downloading file", e);
-            }
-        } else {
-            log.error("File status " + convertRequest.getState() + " is not converted");
-            throw new RuntimeException("File status " + convertRequest.getState() + " is not converted");
-        }
     }
 }
